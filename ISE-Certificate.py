2cd7d7e0457693ca31e1fae8ef6902665632d6ec
Ian Tony Birchall <martymcmadness@gmail.com>
2023-08-30 16:49:14 +0100

    Fix node read and .json file requirement
    Fixes issue of having a static node name to make script more scaleable
    Fixes inefficiency of needing to write to file
    includes some testing data for use

--- ISE-Certificate.py
+++ ISE-Certificate.py
@@ -5,6 +5,7 @@ import time
 
 currentTime = datetime.now().strftime("%Y-%m-%d")
 
+#Credentials/login information for the DevnetSandbox ISE with Ansible
 api = IdentityServicesEngineAPI(username='admin',
                                 password='C1sco12345',
                                 uses_api_gateway=True,
@@ -15,23 +16,35 @@ api = IdentityServicesEngineAPI(username='admin',
                                 uses_csrf_token=False
                                 )
 
-#Current node name (ToDo: update to automatically pull node name)
-node= "ise-1"
+#Blank list for node names to be appended too 
+nodeList = []
+#Testing data for nodes##
+#testgetnode = {'response': [{'hostname': 'ise-1', 'fqdn': 'ise-1.lab.devnetsandbox.local', 'ipAddress': '10.10.20.77', 'roles': ['Standalone'], 'services': ['Profiler', 'Session'], 'nodeStatus': 'Connected'},{'hostname': 'ise-2', 'fqdn': 'ise-2.lab.devnetsandbox.local', 'ipAddress': '10.10.20.78', 'roles': ['Standalone'], 'services': ['Profiler', 'Session'], 'nodeStatus': 'Connected'}], 'version': '1.0.0'}
+#for node in testgetnode["response"]:
+#    nodename = node["hostname"]
+#    nodeList.append(nodename)
 
-#API call that gets the JSON back from ISE with all Certificate information
-get_sysCerts = api.certificates.get_system_certificates(host_name=node).response
+#API call to get a list of all node Names for use in certificate call. 
+getNodes= api.node_deployment.get_nodes().response
+for getNode in getNodes["response"]:
+    nodename = getNode["hostname"]
+    nodeList.append(nodename)
 
-#Writes API return in to a .json file
-json_object = json.dumps(get_sysCerts, indent=2)
-with open("SystemCertificates.json", "w") as outfile:
-    outfile.write(json_object)
+##API call that gets the JSON back from ISE with all Certificate information
+for node in nodeList:
+    get_sysCerts = api.certificates.get_system_certificates(host_name=node).response
 
-#Reads .json file to be able to pull appropriate responses 
-with open("SystemCertificates.json", "r") as file:
-    data = json.load(file)
+### Not Required, this section can write the content of the API get request in to a json file. 
+### And then read from it. You can also use the Stystem Certificates.json as for testing 
+##Writes API return in to a .json file
+#json_object = json.dumps(get_sysCerts, indent=2)
+#with open("SystemCertificates.json", "w") as outfile:
+#    outfile.write(json_object)
+##Reads .json file to be able to pull appropriate responses 
+#with open("SystemCertificates.json", "r") as file:
+#    data = json.load(file)
 
-
-response = data['response']
+response = get_sysCerts['response']
 for keys in response:
     #Sets interesting value Variables using Key pair name
     #This be used to grab other information also as long as you know the name
@@ -51,7 +64,7 @@ for keys in response:
     delta = a-b
     days = int(delta / 86400)
     
-    #Print receipt with details
+    # Print receipt with details
     print ("######################################################################")
     print ("The certifiate for", node, "Is about to Expire in", days, "days")
     print ("This certifiate is used by", usedBy)
